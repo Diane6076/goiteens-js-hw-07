5968d6e6a0fe951e11c9b51b24d7f210417e85a5
diana <dskripka005@gmail.com>
2023-07-19 18:27:22 +0300

    index

--- js/main2.js
+++ js/main2.js
@@ -1,22 +1,21 @@
-
 //2
 document.addEventListener('DOMContentLoaded', function () {
-    const daysOfWeek = ['понеділок', 'вівторок', 'середа', 'четвер', 'пʼятниця', 'субота', 'неділя'];
+const daysOfWeek = ['понеділок', 'вівторок', 'середа', 'четвер', 'пʼятниця', 'субота', 'неділя'];
     
-    const dayInput = document.getElementById('dayInput');
-    const buttonDays = document.getElementById('buttonDays');
-    const messageDays = document.getElementById('messageDays');
+const dayInput = document.getElementById('dayInput');
+const buttonDays = document.getElementById('buttonDays');
+const messageDays = document.getElementById('messageDays');
     
-    buttonDays.addEventListener('click', function () {
-    const inputDay = dayInput.value.trim().toLowerCase();
+buttonDays.addEventListener('click', function () {
+const inputDay = dayInput.value.trim().toLowerCase();
     
-    if (daysOfWeek.includes(inputDay)) {
-    if (inputDay === 'субота' || inputDay === 'неділя') {
-        messageDays.textContent = `День "${inputDay}" - вихідний!`;
-    } else {
-        messageDays.textContent = `День "${inputDay}" - робочий.`; }
-    } else {
-        messageDays.textContent = 'Введено некоректний день тижня. Спробуйте ще раз.';
-    }
-        });
-    });
+if (daysOfWeek.includes(inputDay)) {
+if (inputDay === 'субота' || inputDay === 'неділя') {
+    messageDays.textContent = `День "${inputDay}" - вихідний!`;
+} else {
+    messageDays.textContent = `День "${inputDay}" - робочий.`; }
+} else {
+    messageDays.textContent = 'Введено некоректний день тижня. Спробуйте ще раз.';
+}
+});
+});
